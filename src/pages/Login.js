9cb9e9ae17f7ae1e72490665e9cdfa266ec852e9
intera-augustosimplicio <57759278+intera-augustosimplicio@users.noreply.github.com>
2020-05-02 13:55:24 -0300

    Commit mascara de senha
    Commit mascara de senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,9 +15,11 @@ export default function Login() {
 
   const [offset] = useState(new Animated.ValueXY({x:0, y:80}));
   const [opacity] = useState(new Animated.Value(0));
-  const [logo] = useState(new Animated.ValueXY({x:130, y: 155}));
+  const [logo] = useState(new Animated.ValueXY({x:145, y: 155}));
 
   useEffect(()=> {
+    KeyboardDidShowListener = Keyboard.addListener('KeyboardDidShow', KeyboardDidShow);
+    KeyboardDidHideListener = Keyboard.addListener('KeyboardDidHide', KeyboardDidHide);
 
     Animated.parallel([
         Animated.spring(offset.y, {
@@ -35,6 +37,33 @@ export default function Login() {
 
   }, []);
 
+  function KeyboardDidShow() {
+    alert("Abero")
+    Animated.parallel([
+        Animated.timing(logo.x, {
+            toValue: 55,
+            duration: 100,
+        }),
+        Animated.timing(logo.y, {
+            toValue: 65,
+            duration: 100,
+        }),
+    ]).start();
+  }
+
+  function KeyboardDidHide() {
+    alert("fechado")
+    Animated.parallel([
+        Animated.timing(logo.x, {
+            toValue: 145,
+            duration: 100,
+        }),
+        Animated.timing(logo.y, {
+            toValue: 155,
+            duration: 100,
+        }),
+    ]).start();
+  }
 
     return (
         <KeyboardAvoidingView style={styles.background}>
@@ -43,7 +72,7 @@ export default function Login() {
                 <Animated.Image 
                 source={require('../assets/hat.png')}
                 style={
-                    { width: 200, height: 200}
+                    { width: logo.x, height: logo.y}
                 }
                 />
             </View>
@@ -68,6 +97,7 @@ export default function Login() {
                 
                 <TextInput 
                     placeholder="Senha" 
+                    secureTextEntry={true}
                     autoCorrect={false} 
                     onChangeText={()=>{}}
                     style={styles.input}
